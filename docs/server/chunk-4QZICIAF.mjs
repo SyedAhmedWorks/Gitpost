5cf7df4fe7efa910e7c5825558624bdf311d8201
Ahmed Hashmi <ahmed.hashmi@contour-software.com>
2024-10-24 16:10:06 +0500

    we are live

---
+++ docs/server/chunk-4QZICIAF.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as r,b as e,c as p,d as n,e as m,j as a,m as f,o as s,p as l,s as c,y as g}from"./chunk-VIONULFM.mjs";var o=class i{title="Gitpost";static \u0275fac=function(t){return new(t||i)};static \u0275cmp=r({type:i,selectors:[["app-root"]],standalone:!0,features:[m],decls:4,vars:0,template:function(t,b){t&1&&(e(0,"h2"),n(1,"Testing SQA"),p(),e(2,"p"),n(3,` Deploying angular web on Github page. Hello World!
+`),p())}})};var d=[];var u={providers:[a({eventCoalescing:!0}),g(d),l()]};var A={providers:[c()]},C=f(u,A);var x=()=>s(o,C),Z=x;export{Z as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4QZICIAF.mjs";import"./chunk-VIONULFM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
